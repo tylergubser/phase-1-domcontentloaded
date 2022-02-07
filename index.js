31db851e9dce46ee55266476de3df9cf13d0370c
tylergubser <tylergubser2200@gmail.com>
2022-02-07 17:18:45 -0600

    commit tree 760467b53ad620fb45df33df69732aeb32ff6f16

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function(id) {
+    id = document.getElementById("text");
+    id.textContent = "This is really cool!";
+    return id.textContent;
+
+  });
